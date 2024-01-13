7608ceef5d421dff2310c35220d1e20a7c3c8631
riskiapriliansyah <riski.apriliansyah25@gmail.com>
2024-01-13 14:19:56 +0800

    asd

--- app/Http/Controllers/BestBuyController.php
+++ app/Http/Controllers/BestBuyController.php
@@ -77,4 +77,45 @@ class BestBuyController extends BaseController
             return $this->sendError("", $ex->getMessage());
         }
     }
+
+    public function storeBestBuyDep(Request $request)
+    {
+        $stock = Stock::where("bara1", $request->bara1)->first();
+        $tsatuan = Tsatuan::where("bara1", $request->bara1)->first();
+
+        try {
+            DB::beginTransaction();
+            $best = new BestBuy;
+            $best->bara = $request->bara;
+            $best->bara1 = $request->bara1;
+            $best->hbest = $request->hbest;
+            $best->dbest = $request->dbest;
+            $best->dbest1 = $request->dbest1;
+            $best->best1 = $request->best1;
+            $best->best2 = $request->best2;
+            $best->tipe = "1";
+            $best->save();
+
+            $stock->hbest = $request->hbest;
+            $stock->dbest = $request->dbest;
+            $stock->dbest1 = $request->dbest1;
+            $stock->best1 = $request->best1;
+            $stock->best2 = $request->best2;
+            $stock->save();
+
+            if (isset($tsatuan)) {
+                $tsatuan->hbest = $request->hbest;
+                $tsatuan->dbest = $request->dbest;
+                $tsatuan->dbest1 = $request->dbest1;
+                $tsatuan->best1 = $request->best1;
+                $tsatuan->best2 = $request->best2;
+                $tsatuan->save();
+            }
+            DB::commit();
+            return $this->sendResponse($best, "Berhasil simpan best buy");
+        } catch (Exception $ex) {
+            DB::rollBack();
+            return $this->sendError("", $ex->getMessage());
+        }
+    }
 }
