a9d7f7f3361b285b0665f123917c2cfbb0ed3db9
riskiapriliansyah <riski.apriliansyah25@gmail.com>
2024-02-16 17:18:53 +0800

    asd

--- resources/js/Layouts/Sidebar.jsx
+++ resources/js/Layouts/Sidebar.jsx
@@ -1515,7 +1515,7 @@ export default function Sidebar(props) {
         <>
             <div className="h-screen bg-gradient-to-r from-base-200 via-base-100 to-base-200 w-0 lg:w-64 py-7 fixed left-0">
                 <div className="flex flex-col items-center h-3/4">
-                    <img src="/assets/logo-apollo.png" alt="" width={150} />
+                    {/* <img src="/assets/logo-apollo.png" alt="" width={150} /> */}
                     {role === "admin" ? <AdminBar /> : ""}
                     {role === "super_admin" ? <SuperAdminBar /> : ""}
                 </div>
